699361dbf3c755720ef21927084ca62f5d2b5aaf
AmishaLama <amishalama5@gmail.com>
2023-06-22 09:06:51 +0545

    Hari is in array

--- JavaScript/Array/level2.js
+++ JavaScript/Array/level2.js
@@ -3,3 +3,8 @@
 //true
 const name = 'hari';
 const allUsers = ['hari', 'shyam', 'thakur'];
+for (i = 0; i < allUsers.length; i = i + 1) {
+  if (allUsers[i] == 'hari') {
+    console.log(true);
+  }
+}
