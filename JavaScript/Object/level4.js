048de53da4c6edc17f2e30ed3380f1f298d243a3
AmishaLama <amishalama5@gmail.com>
2023-06-26 18:42:00 +0545

    Generated username subcode subscores

--- JavaScript/Object/level4.js
+++ JavaScript/Object/level4.js
@@ -11,3 +11,29 @@ for (i in obj) {
 let percent = (sum * 100) / 150;
 let p = percent.toFixed(2);
 console.log(p);
+
+//Generate the output as follows
+// {
+//     userName: 'kaylin',
+//     subjectCodes: ['M','S', 'G'],
+//     subjectScores: [23,35,30]
+// }
+const userDetails = {
+  name: 'kaylin',
+  maths: 23,
+  science: 35,
+  gk: 30,
+};
+let subjectCodes = [];
+const sub = Object.keys(userDetails);
+sub.forEach((item) => {
+  if (item != 'name') {
+    subjectCodes.push(item.charAt(0).toUpperCase());
+  }
+});
+const object = {
+  userName: userDetails.name,
+  subjectCodes,
+  subjectScores: Object.values(userDetails).filter(Number),
+};
+console.log(object);
